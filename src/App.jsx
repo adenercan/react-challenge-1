5bc403109ae394672c386256352d5890b44f2175
irem kaya <grosee001@gmail.com>
2025-12-24 17:07:51 +0300

    app.jsx düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-
+import { useState } from "react";
+import Start from "./components/start";
+import Quiz from "./components/quiz";
+import Result from "./components/result";
 function App() {
+  const [screen, setScreen] = useState("start");
+  const [score, setScore] = useState(0);
   
   return (
     <>
-    <h1>hello world</h1>
+    <div className="app">
+
+    {screen === "start" && (
+      <div>
+        <h1>Frontend Quiz</h1>
+        <p>Test your frontend knowledge</p>
+
+        <button onClick={() => setScreen("quiz")}>
+          Start Quiz
+        </button>
+      </div>
+    )}
+
+    {screen === "quiz" && (
+      <div>
+        <h1>Quiz Screen</h1>
+
+        <button onClick={() => setScreen("result")}>
+          Finish Quiz
+        </button>
+      </div>
+    )}
+
+    {screen === "result" && (
+      <div>
+        <h1>Result Screen</h1>
+        <p>Score: {score}</p>
+
+        <button onClick={() => {
+          setScore(0);
+          setScreen("start");
+        }}>
+          Restart
+        </button>
+      </div>
+    )}
+
+      </div>
     </>
   )
 }

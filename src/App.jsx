6210b6e17f90d0f77003403fa8a2e20364f385c7
adenercan <adenercan23@gmail.com>
2025-12-24 18:22:07 +0300

    hala bilmiyorum

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-=======
 import { useState } from "react";
 import Start from "./components/start";
 import Quiz from "./components/quiz";
 import Result from "./components/result";
->>>>>>> b3c5472fcfd0d02dfea34a3e9ec9f0d9af0cb67c
 
 function App() {
 
-<<<<<<< HEAD
-    </>
-  )
-=======
   const [screen, setScreen] = useState("start");
   const [score, setScore] = useState(0);
   const TOTAL_QUESTIONS = 10;
@@ -46,7 +39,6 @@ function App() {
       )}
     </div>
   );
->>>>>>> b3c5472fcfd0d02dfea34a3e9ec9f0d9af0cb67c
 }
 
 export default App;

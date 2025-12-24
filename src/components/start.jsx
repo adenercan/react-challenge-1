6ab0619721bbd3f0d0b607c54f545755046b7544
adenercan <adenercan23@gmail.com>
2025-12-25 02:09:47 +0300

    düzenlendi

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -29,17 +29,15 @@ function Start({ onStart }){
                 {/* theme toggle gelecek */}
             </div>
 
-            {/* başlık */}
             <div className="px-8 pt-6">
                 <h1 className="text-3xl font-normal ">Welcome to the</h1>
                 <h1 className="text-3xl font-bold">Frontend Quiz!</h1>
                 <p className="italic text-[#626C7F] ">Pick a subject to get started</p>
             </div>
 
-            {/* buttonlar */}
             <div className="flex justify-center items-center flex-col p-6 gap-3" >
-                {subjects.map((subject) => (
-                    <button className="flex items-center w-80 p-3 bg-white rounded-xl gap-4" onClick={() => onStart(subject.name)}>
+                {subjects.map((subject, index) => (
+                    <button key={index} className="flex items-center w-80 p-3 bg-white rounded-xl gap-4" onClick={() => onStart(subject.name)}>
                         <div className={`w-10 h-10 ${subject.bgColor} rounded-md flex justify-center items-center flex-row p-1`}>
                             <img className="" src={subject.icon} alt="" />
                         </div>{subject.name}
